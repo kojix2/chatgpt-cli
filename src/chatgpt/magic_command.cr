26597cacbd920182c337ece76d334814f0a8ff72
kojix2 <2xijok@gmail.com>
2023-03-23 23:15:26 +0900

    Refactoring with ChatGPT4

--- src/chatgpt/magic_command.cr
+++ src/chatgpt/magic_command.cr
@@ -11,37 +11,71 @@ module ChatGPT
     def run
       case command
       when "debug"
-        DEBUG_FLAG[0] = !DEBUG_FLAG[0]
-        puts "Debug mode: #{DEBUG_FLAG[0]}".colorize(:yellow)
+        debug_mode_toggle
       when "system"
-        data.messages.each do |msg|
-          if msg["role"] == "system"
-            puts msg["content"].colorize(:yellow)
-          end
-        end
+        show_system_messages
       when /system\s+(.+)/
-        if data.messages.empty?
-          data.messages << {"role" => "system", "content" => $1}
-        elsif data.messages[0]["role"] == "system"
-          data.messages[0]["content"] = $1
-        else
-          data.messages.unshift({"role" => "system", "content" => $1})
-        end
-        puts "Set system message to #{$1}".colorize(:yellow)
+        set_system_messages($1)
       when "clear"
-        data.messages.clear
-        puts "Cleared".colorize(:yellow)
+        clear_messages
       when "data"
-        puts data.to_json.colorize(:yellow)
+        show_data_json
       when "saveall"
-        File.write("chatgpt.json", data.to_json)
-        puts "Saved to chatgpt.json".colorize(:yellow)
+        save_all_to_json
       when /save\s+(.+)/
-        File.write($1, data.messages[-1]["content"])
-        puts "Saved to #{$1}".colorize(:yellow)
+        save_to_file($1)
       else
-        STDERR.puts "Error: Unknown magic command: #{command}".colorize(:yellow).mode(:bold)
+        unknown_command_error
       end
+    end
+
+    private
+
+    def debug_mode_toggle
+      DEBUG_FLAG[0] = !DEBUG_FLAG[0]
+      puts "Debug mode: #{DEBUG_FLAG[0]}".colorize(:yellow)
+    end
+
+    def show_system_messages
+      data.messages.each do |msg|
+        if msg["role"] == "system"
+          puts msg["content"].colorize(:yellow)
+        end
+      end
+    end
+
+    def set_system_messages(message)
+      if data.messages.empty?
+        data.messages << {"role" => "system", "content" => message}
+      elsif data.messages[0]["role"] == "system"
+        data.messages[0]["content"] = message
+      else
+        data.messages.unshift({"role" => "system", "content" => message})
+      end
+      puts "Set system message to #{message}".colorize(:yellow)
+    end
+
+    def clear_messages
+      data.messages.clear
+      puts "Cleared".colorize(:yellow)
+    end
+
+    def show_data_json
+      puts data.to_json.colorize(:yellow)
+    end
+
+    def save_all_to_json
+      File.write("chatgpt.json", data.to_json)
+      puts "Saved to chatgpt.json".colorize(:yellow)
+    end
+
+    def save_to_file(file_name)
+      File.write(file_name, data.messages[-1]["content"])
+      puts "Saved to #{file_name}".colorize(:yellow)
+    end
+
+    def unknown_command_error
+      STDERR.puts "Error: Unknown magic command: #{command}".colorize(:yellow).mode(:bold)
     end
   end
 end
