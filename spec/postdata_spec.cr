35ac8d3c370e32d1d63f0404c3b81efc2a7513ec
kojix2 <2xijok@gmail.com>
2023-03-23 20:40:41 +0900

    Add minimum test

---
+++ spec/postdata_spec.cr
@@ -0,0 +1,34 @@
+require "./spec_helper"
+require "../src/postdata"
+
+
+
+module ChatGPT
+  data = PostData.new(messages: [{"role" => "system", "content" => "Set some context"}])
+
+  describe PostData do
+    it "should have system message" do
+      data.messages.should contain({"role" => "system", "content" => "Set some context"})
+    end
+
+    it "adds user message correctly" do
+      data.messages << {"role" => "user", "content" => "What's the capital of France?"}
+      data.messages.should contain({"role" => "user", "content" => "What's the capital of France?"})
+    end
+
+    it "generates correct number of edits" do
+      data.n = 2
+      data.n.should eq(2)
+    end
+
+    it "sets temperature correctly" do
+      data.temperature = 0.8
+      data.temperature.should eq(0.8)
+    end
+
+    it "sets top_p correctly" do
+      data.top_p = 0.9
+      data.top_p.should eq(0.9)
+    end
+  end
+end
--- src/chatgpt.cr
+++ src/chatgpt.cr
@@ -9,6 +9,7 @@ require "spinner"
 require "lexbor"
 
 require "./file_extensions"
+require "./postdata"
 
 PROGRAM_VERSION = "0.1.0"
 DEBUG_FLAG      = [false]
@@ -28,22 +29,7 @@ def words(parser)
     .map(&.strip.gsub(/\s{2,}/, " "))      # remove extra spaces
 end
 
-struct PostData
-  include JSON::Serializable
-  property model : String
-  property messages : Array(Hash(String, String))
-  property temperature : Float64
-  property top_p : Float64
-  property n : Int32
 
-  def initialize
-    @model = "gpt-3.5-turbo"
-    @messages = [] of Hash(String, String)
-    @n = 1
-    @temperature = 1.0
-    @top_p = 1.0
-  end
-end
 
 data = PostData.new
 
