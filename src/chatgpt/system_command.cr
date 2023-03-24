00fd712984d2047ec3d5bdb92b7d012f289b32aa
kojix2 <2xijok@gmail.com>
2023-03-24 17:02:19 +0900

    Rename variable

--- src/chatgpt/system_command.cr
+++ src/chatgpt/system_command.cr
@@ -1,9 +1,9 @@
 module ChatGPT
   class SystemCommand
-    def try_run(message)
-      if message.starts_with? "!"
-        cmd = message[1..-1].strip
-        run(cmd)
+    def try_run(input_message)
+      if input_message.starts_with? "!"
+        extracted_command = input_message[1..-1].strip
+        run(extracted_command)
         true
       else
         false
@@ -11,12 +11,12 @@ module ChatGPT
     end
 
     def run(command)
-      output = `#{command}`
+      command_output = `#{command}`
       if $?.success?
-        puts output.colorize(:yellow)
+        puts command_output.colorize(:yellow)
       else
         STDERR.puts "Error: Command failed: #{command}".colorize(:yellow).mode(:bold)
-        STDERR.puts output.colorize(:yellow)
+        STDERR.puts command_output.colorize(:yellow)
       end
     end
   end
