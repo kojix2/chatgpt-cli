4e8fba96ba865726a29a0c583fe8998766df4c2f
kojix2 <2xijok@gmail.com>
2023-03-24 16:17:51 +0900

    Refactored by ChatGPT

--- src/chatgpt.cr
+++ src/chatgpt.cr
@@ -1,5 +1,3 @@
-# https://platform.openai.com/docs/api-reference/chat
-
 require "option_parser"
 require "http/client"
 require "json"
@@ -24,76 +22,62 @@ parser.parse
 data = parser.data
 
 client = ChatGPT::Client.new
-
 system_cmd = ChatGPT::SystemCommand.new
 magic_cmd = ChatGPT::MagicCommand.new(data, key: "%")
 
+def process_url_substitution(match)
+  url = match[3..-2].strip
+  url = "https://" + url unless url.starts_with?("http")
+  compressed_text = words(Lexbor::Parser.new(HTTP::Client.get(url).body.to_s)).join("|")
+
+  <<-CODE_BLOCK
+    ### #{url}
+
+    ```
+    #{compressed_text}
+    ```
+
+    That's all for the #{url}
+  CODE_BLOCK
+end
+
+def process_file_substitution(path, match)
+  extname = File.extname(path)
+  basename = File.basename(path)
+  format_name = ChatGPT::FILE_EXTENSIONS.fetch(extname, "")
+
+  if File.exists?(path)
+    <<-CODE_BLOCK
+      ### #{basename}
+
+      ```#{format_name}
+      #{File.read(path)}
+      ```
+
+      That's all for the #{basename}
+    CODE_BLOCK
+  else
+    STDERR.puts "Error: File not found: #{path}".colorize(:yellow).mode(:bold)
+    match
+  end
+end
+
 loop do
-  # Get input from the user
   msg = Readline.readline("> ", true)
   break if msg.nil?
-
-  # Enter
   next if msg.empty?
-
-  # Exit
-  break if msg == "exit"
-  break if msg == "quit"
-
-  # Run command if the message starts with `!`
+  break if ["exit", "quit"].includes?(msg)
   next if system_cmd.try_run(msg)
 
-  # Run magic command if the message starts with `%`
-  if data2 = magic_cmd.try_run(msg, data)
-    data = data2 if data2.is_a?(ChatGPT::PostData) # FIXME
+  if modified_data = magic_cmd.try_run(msg, data)
+    data = modified_data if modified_data.is_a?(ChatGPT::PostData)
     next
   end
 
-
-  # Replace %%{...} with the contents of the url
-  msg = msg.gsub(/%%{.+?}/) do |match|
-    url = match[3..-2].strip
-    unless url.starts_with? "http"
-      url = "https://" + url
-    end
-    compressed_text = words(Lexbor::Parser.new(HTTP::Client.get(url).body.to_s)).join("|")
-    str = <<-CODE_BLOCK
-      ### #{url}
-
-      ```
-      #{compressed_text}
-      ```
-
-      That's all for the #{url}
-    CODE_BLOCK
-    "\n\n#{str}\n\n"
-  end
-
-  # Replace %{...} with the contents of the file
-  msg = msg.gsub(/%{.+?}/) do |match|
-    path = match[2..-2].strip
-    extname = File.extname(path)
-    basename = File.basename(path)
-    format_name = ChatGPT::FILE_EXTENSIONS.fetch(extname, "")
-    if File.exists?(path)
-      str = <<-CODE_BLOCK
-        ### #{basename}
-  
-        ```#{format_name}
-        #{File.read(path)}
-        ```
-  
-        That's all for the #{basename}
-        CODE_BLOCK
-      "\n\n#{str}\n\n"
-    else
-      STDERR.puts "Error: File not found: #{path}".colorize(:yellow).mode(:bold)
-      next match
-    end
-  end
+  msg = msg.gsub(/%%{.+?}/) { |match| process_url_substitution(match) }
+  msg = msg.gsub(/%{.+?}/) { |match| process_file_substitution(match[2..-2].strip, match) }
 
   data.messages << {"role" => "user", "content" => msg}
-
   response = client.send_chat_request(data)
   response_data = JSON.parse(response.body)
 
