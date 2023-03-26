0646bce24f894820c483841680cfd7dd32e961b8
kojix2 <2xijok@gmail.com>
2023-03-26 23:40:50 +0900

    Refactored ChatGPT::InputSubstitutor

--- src/chatgpt/input_substitutor.cr
+++ src/chatgpt/input_substitutor.cr
@@ -2,7 +2,6 @@ require "./webpage_compressor"
 
 module ChatGPT
   class InputSubstitutor
-
     def initialize(@system_command_runner : SystemCommandRunner)
     end
 
@@ -47,26 +46,42 @@ module ChatGPT
     def file(input_msg, file_pattern)
       input_msg.gsub(file_pattern) do |file_match|
         file_pattern = $1.strip
-
-        file_paths = Dir.glob(file_pattern)
-        if file_paths.empty?
-          STDERR.puts "Warning: No files found matching: #{file_pattern} leave it as it is".colorize(:yellow).mode(:bold)
-          next file_match
+        formatted_file_contents = get_formatted_file_contents(file_match, file_pattern)
+        if formatted_file_contents.is_a?(Array)
+          formatted_file_contents.join("\n")
+        else
+          formatted_file_contents # It's a string (file_match)
         end
+      end
+    end
 
-        content_blocks = file_paths.map do |file_path|
-          extname = File.extname(file_path)
-          basename = File.basename(file_path)
-          begin
-            contents = File.read(file_path)
-          rescue ex
-            # This should not happen because we already checked the file exists with Dir.glob
-            STDERR.puts "Error: #{ex}\nFailed to read file: #{file_path}".colorize(:yellow).mode(:bold)
-            contents = "# Error: Failed to read file: #{file_path}"
-          end
-          format_name = FILE_EXTENSIONS.fetch(extname, "")
+    def url(input_msg, url_pattern)
+      input_msg.gsub(url_pattern) do |url_match|
+        url_pattern = $1.strip
+        fetch_and_compress_url_contents(url_match, url_pattern)
+      end
+    end
 
-          <<-CODE_BLOCK
+    private def get_formatted_file_contents(file_match, file_pattern)
+      file_paths = Dir.glob(file_pattern)
+      if file_paths.empty?
+        STDERR.puts "Warning: No files found matching: #{file_pattern} leave it as it is".colorize(:yellow).mode(:bold)
+        return file_match
+      end
+
+      file_paths.map do |file_path|
+        extname = File.extname(file_path)
+        basename = File.basename(file_path)
+        begin
+          contents = File.read(file_path)
+        rescue ex
+          # This should not happen because we already checked the file exists with Dir.glob
+          STDERR.puts "Error: #{ex}\nFailed to read file: #{file_path}".colorize(:yellow).mode(:bold)
+          contents = "# Error: Failed to read file: #{file_path}"
+        end
+        format_name = FILE_EXTENSIONS.fetch(extname, "")
+
+        <<-CODE_BLOCK
           ### #{basename}
 
           ```#{format_name}
@@ -76,30 +91,25 @@ module ChatGPT
           That's all for the #{basename}
 
           CODE_BLOCK
-        end
-
-        content_blocks.join("\n")
       end
     end
 
-    def url(input_msg, url_pattern)
-      input_msg.gsub(url_pattern) do |url_match|
-        url = $1.strip
-        url = "https://" + url unless url.starts_with?("http")
-        begin
-          response = HTTP::Client.get(url)
-        rescue ex
-          STDERR.puts "Warning: Failed to fetch url: #{url} leave it as it is".colorize(:yellow).mode(:bold)
-          STDERR.puts ex.message.colorize(:yellow)
-          next url_match
-        end
-        unless response.success?
-          STDERR.puts "Warning: Failed to fetch url: #{url} leave it as it is".colorize(:yellow).mode(:bold)
-          next url_match
-        end
-        compressed_text = words(Lexbor::Parser.new(HTTP::Client.get(url).body.to_s)).join("|")
+    private def fetch_and_compress_url_contents(url_match, url)
+      url = "https://" + url unless url.starts_with?("http")
+      begin
+        response = HTTP::Client.get(url)
+      rescue ex
+        STDERR.puts "Warning: Failed to fetch url: #{url} leave it as it is".colorize(:yellow).mode(:bold)
+        STDERR.puts ex.message.colorize(:yellow)
+        return url_match
+      end
+      unless response.success?
+        STDERR.puts "Warning: Failed to fetch url: #{url} leave it as it is".colorize(:yellow).mode(:bold)
+        return url_match
+      end
+      compressed_text = words(Lexbor::Parser.new(HTTP::Client.get(url).body.to_s)).join("|")
 
-        <<-CODE_BLOCK
+      <<-CODE_BLOCK
         ### #{url}
 
         ```
@@ -108,7 +118,6 @@ module ChatGPT
 
         That's all for the #{url}
         CODE_BLOCK
-      end
     end
   end
 end
