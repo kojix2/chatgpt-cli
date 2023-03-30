6d036eea70842246a091a1c25ac2de6cb5ec2139
kojix2 <2xijok@gmail.com>
2023-03-30 20:37:57 +0900

    Fix project name

--- src/chatgpt/cli/version.cr
+++ src/chatgpt/cli/version.cr
@@ -1,6 +1,6 @@
 module ChatGPT
   class CLI
-    VERSION    = "0.1.2"
+    VERSION    =  {{ `shards version #{__DIR__}`.chomp.stringify }}
     SOURCE_URL = "https://github.com/kojix2/chatgpt-cli"
   end
 end
