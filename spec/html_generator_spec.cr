2ff365d78d70c668ee36ac830cbae9eb88d9f231
kojix2 <2xijok@gmail.com>
2023-04-03 17:01:18 +0900

    Fix html_generator

--- spec/html_generator_spec.cr
+++ spec/html_generator_spec.cr
@@ -1,39 +1,37 @@
 require "./spec_helper"
 require "../src/chatgpt/html_generator"
 
-module ChatGPT
-  messages = [
-    {
-      "role"    => "system",
-      "content" => "You are a cat. Listen, you are a cat. You are a cat.",
-    },
-    {
-      "role"    => "user",
-      "content" => "Shouldn't humans and artificial intelligence be complementary to each other?",
-    },
-    {
-      "role"    => "assistant",
-      "content" => "Meow!",
-    },
-    {
-      "role"    => "user",
-      "content" => "The more artificial intelligence evolves, the more ethical issues and moral dilemmas we will face. It's important to always maintain control over its evolution, don't you think?",
-    },
-    {
-      "role"    => "assistant",
-      "content" => "Meow!",
-    },
-  ]
+messages = [
+  {
+    "role"    => "system",
+    "content" => "You are a cat. Listen, you are a cat. You are a cat.",
+  },
+  {
+    "role"    => "user",
+    "content" => "Shouldn't humans and artificial intelligence be complementary to each other?",
+  },
+  {
+    "role"    => "assistant",
+    "content" => "Meow!",
+  },
+  {
+    "role"    => "user",
+    "content" => "The more artificial intelligence evolves, the more ethical issues and moral dilemmas we will face. It's important to always maintain control over its evolution, don't you think?",
+  },
+  {
+    "role"    => "assistant",
+    "content" => "Meow!",
+  },
+]
 
-  describe HtmlGenerator do
-    it "generates html" do
-      data = PostData.new
-      data.messages = messages
-      generator = HtmlGenerator.new(data)
-      expected_html = File.read("#{__DIR__}/fixtures/meow.html")
-      # File.write("#{__DIR__}/meow.html", generator.to_s)
-      actual_html = generator.to_s
-      actual_html.should eq(expected_html)
-    end
+describe ChatGPT::HtmlGenerator do
+  it "generates html" do
+    data = ChatGPT::PostData.new
+    data.messages = messages
+    generator = ChatGPT::HtmlGenerator.new(data)
+    expected_html = File.read("#{__DIR__}/fixtures/meow.html")
+    # File.write("#{__DIR__}/meow.html", generator.to_s)
+    actual_html = generator.to_s
+    actual_html.should eq(expected_html)
   end
 end
--- src/chatgpt/html_generator.cr
+++ src/chatgpt/html_generator.cr
@@ -1,57 +1,59 @@
 require "ecr"
 require "html"
 
-class HtmlGenerator
-  class HtmlGenerationError < Exception; end
+module ChatGPT
+  class HtmlGenerator
+    class HtmlGenerationError < Exception; end
 
-  class Message
-    @message : Hash(String, String)
+    class Message
+      @message : Hash(String, String)
 
-    def initialize(@message)
-    end
+      def initialize(@message)
+      end
 
-    def role
-      @message["role"]
-    end
+      def role
+        @message["role"]
+      end
 
-    def content
-      HTML.escape(@message["content"])
-    end
+      def content
+        HTML.escape(@message["content"])
+      end
 
-    def avatar_class
-      case role
-      when "user", "system" # FIXME system
-        "avatar human"
-      when "assistant"
-        "avatar gpt"
-      else
-        raise HtmlGenerationError.new("Unknown role: #{role}")
+      def avatar_class
+        case role
+        when "user", "system" # FIXME system
+          "avatar human"
+        when "assistant"
+          "avatar gpt"
+        else
+          raise HtmlGenerationError.new("Unknown role: #{role}")
+        end
+      end
+
+      def direction
+        case role
+        when "user", "system" # FIXME system
+          "from"
+        when "assistant"
+          "to"
+        else
+          raise HtmlGenerationError.new("Unknown role: #{role}")
+        end
+      end
+
+      def avatar_image
+        raise NotImplementedError
       end
     end
 
-    def direction
-      case role
-      when "user", "system" # FIXME system
-        "from"
-      when "assistant"
-        "to"
-      else
-        raise HtmlGenerationError.new("Unknown role: #{role}")
+    @messages : Array(Message)
+
+    def initialize(data)
+      @messages = data.messages.map do |message|
+        Message.new(message)
       end
     end
 
-    def avatar_image
-      raise NotImplementedError
-    end
+    ECR.def_to_s "#{__DIR__}/output.html.ecr"
   end
-
-  @messages : Array(Message)
-
-  def initialize(data)
-    @messages = data.messages.map do |message|
-      Message.new(message)
-    end
-  end
-
-  ECR.def_to_s "#{__DIR__}/output.html.ecr"
 end
