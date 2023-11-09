1d277e10f715ad513a4da67bc95672e21ea17169
kojix2 <2xijok@gmail.com>
2023-11-09 09:54:29 +0900

    Make !{} capture both stdout and stderr Fix !{ cmd } should capture both STDOUT and STDERR ? #14

--- src/chatgpt/substitutor/command.cr
+++ src/chatgpt/substitutor/command.cr
@@ -10,7 +10,21 @@ module ChatGPT
       def command(input_msg, command_pattern)
         input_msg.gsub(command_pattern) do |command_match|
           command_pattern = $1.strip
-          command_output = `#{command_pattern}`
+          command_output = run_command(command_pattern)
+        end
+      end
+
+      # Mix stderror and stdout
+      # MEMO: should be move to SystemComannd class?
+      private def run_command(command)
+        std_oe = IO::Memory.new
+        begin
+          status = Process.run(command, shell: true, output: std_oe, error: std_oe)
+          unless status.success?
+            STDERR.puts "Warning: failed(#{status.exit_code}): #{command}"._colorize(:warning, :bold)
+          end
+        rescue ex
+          STDERR.puts "Error: Failed to run command: #{command}"._colorize(:warning, :bold)
         end
       end
     end
