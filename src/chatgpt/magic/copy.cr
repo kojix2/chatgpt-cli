bf75e85710759c815570fac3a24049d85c5fea01
kojix2 <2xijok@gmail.com>
2023-11-21 13:26:25 +0900

    Add easyclip as a dependency and use it for copying content to clipboard

--- src/chatgpt/magic/copy.cr
+++ src/chatgpt/magic/copy.cr
@@ -1,3 +1,4 @@
+require "easyclip"
 require "./base"
 
 module ChatGPT
@@ -19,22 +20,9 @@ module ChatGPT
         n = n.to_i
         message = get_message(n) # Defined in Base
         return true if message.nil?
-        tempfile = File.tempfile("chatgpt") do |f|
-          f.print(message.fetch("content", ""))
-        end
-        copy_to_clipboard(tempfile.path)
-        tempfile.delete
+        content = message["content"]
+        EasyClip.copy(content)
         true
-      end
-
-      private def copy_to_clipboard(path)
-        {% if flag?(:darwin) %}
-          system "pbcopy < #{path}"
-        {% elsif flag?(:unix) %}
-          system "xsel -ib < #{path}"
-        {% elsif flag?(:win32) %}
-          system "clip < #{path}"
-        {% end %}
       end
     end
   end
